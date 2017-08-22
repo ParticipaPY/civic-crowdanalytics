4a693614e2e77fb477c511faec447c265471b05f
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-08-22 16:19:01 -0400

    Add dropdown user menu

--- frontoffice/src/components/Header.vue
+++ frontoffice/src/components/Header.vue
@@ -6,6 +6,35 @@
         <a class="nav-link navbar-toggler sidebar-toggler" href="#" @click="sidebarToggle">&#9776;</a>
       </li>
     </ul>
+    <ul class="nav navbar-nav ml-auto">
+      <li class="nav-item d-md-down-none">
+        <a class="nav-link" href="#" onclick="alert('You have five new messages');"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a>
+      </li>
+      <dropdown size="nav" class="nav-item">
+        <span slot="button">
+          <img src="static/img/avatars/6.jpg" class="img-avatar" alt="avatar-user">
+          <span class="d-md-down-none">User</span>
+        </span>
+        <div slot="dropdown-menu"class="dropdown-menu dropdown-menu-right">
+
+          <div class="dropdown-header text-center"><strong>Account</strong></div>
+
+          <a class="dropdown-item" href="#"><i class="fa fa-file"></i> Projects<span class="badge badge-primary">10</span></a>
+          <a class="dropdown-item" href="#"><i class="fa fa-bell-o"></i> Updates<span class="badge badge-success">3</span></a>
+          <a class="dropdown-item" href="#"><i class="fa fa-envelope-o"></i> Messages<span class="badge badge-danger">5</span></a>
+          <a class="dropdown-item" href="#"><i class="fa fa-comments"></i> Comments<span class="badge badge-warning">4</span></a>
+
+          <div class="dropdown-header text-center"><strong></strong></div>
+
+          <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Profile</a>
+          <a class="dropdown-item" href="#"><i class="fa fa-wrench"></i> Settings</a>
+          <div class="divider"></div>
+          <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> Logout</a>
+        </div>
+      </dropdown>
+      <li class="nav-item d-md-down-none">
+      </li>
+    </ul>
   </navbar>
 </template>
 <script>
