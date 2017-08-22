deb5ad96647e5668f948b557f5aae7ac385ec08a
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-08-22 16:19:56 -0400

    Remove unused code

--- frontoffice/src/views/charts/BarChart.vue
+++ frontoffice/src/views/charts/BarChart.vue
@@ -9,22 +9,18 @@ export default Bar.extend({
       datasets: [
         {
           label: 'Categories',
-          data: [12, 19, 3, 5, 2, 3],
+          data: [97, 43, 136, 37],
           backgroundColor: [
             'rgba(255, 99, 132, 0.5)',
             'rgba(54, 162, 235, 0.5)',
             'rgba(255, 206, 86, 0.5)',
-            'rgba(75, 192, 192, 0.5)',
-            'rgba(153, 102, 255, 0.5)',
-            'rgba(255, 159, 64, 0.5)'
+            'rgba(75, 192, 192, 0.5)'
           ],
           borderColor: [
             'rgba(255,99,132,1)',
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
+            'rgba(75, 192, 192, 1)'
           ],
           borderWidth: 1
         }
