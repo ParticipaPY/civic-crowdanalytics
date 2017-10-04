b5780407ffcb1cb46864659abe19951e77146b9b
guidonu <guidonu91@gmail.com>
2017-10-04 15:53:51 -0300

    Add CRUD of User

--- backoffice/backoffice/urls.py
+++ backoffice/backoffice/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from core import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backoffice/core/views.py
+++ backoffice/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from core.models import User
+from rest_framework import viewsets
+from core.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
