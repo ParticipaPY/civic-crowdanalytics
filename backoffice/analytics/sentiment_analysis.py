e6f3112d4530d8d3ca227aea51ed0788d5fac7c0
Marcelo Alcaraz <marce.mmad@gmail.com>
2017-10-18 13:49:39 -0300

    Change parameters and functions names for consistency

--- backoffice/analytics/sentiment_analysis.py
+++ backoffice/analytics/sentiment_analysis.py
@@ -8,13 +8,13 @@ from textblob import TextBlob
 class SentimentAnalyzer():
     '''
     Analyzes the sentiment polarity of a collection of documents.
-    It determines wether the feeling about each text is positive,
+    It determines wether the feeling about each doc is positive,
     negative or neutral
 
     Parameters
     ----------
     neu_inf_lim : float, -0.3 by default
-        If a doc's polarity score is lower than this paramenter, 
+        If a doc's polarity score is lower than this paramenter,
         then the sentiment is considered negative.
         Use values greater than -1 and lower than 0.
 
@@ -24,8 +24,8 @@ class SentimentAnalyzer():
         Use values greater than 0 and lower than 1.
 
     algorithm : string, 'ntlk_vader' by default
-        The algorithm used to calculate the polarity score of a text.
-        Immplemented algorithms: 'ntlk_vader', 'textblob_base' 
+        The algorithm used to calculate the polarity score of a doc.
+        Immplemented algorithms: 'ntlk_vader', 'textblob_base'
     '''
 
     _sia = SentimentIntensityAnalyzer()
@@ -38,51 +38,51 @@ class SentimentAnalyzer():
         self.neu_sup_lim = neu_sup_lim
         self.algorithm = algorithm
 
-    def get_polarity_score(self, text):
+    def get_polarity_score(self, doc):
         '''
-        Returns the polarity score for a given text.
+        Returns the polarity score for a given doc.
         This score ranges from -1 to 1, were -1 is extreme negative
         and 1 means extreme positive.
 
         '''
 
         if self.algorithm == "nltk_vader":
-            return self._sia.polarity_scores(text)["compound"]
+            return self._sia.polarity_scores(doc)["compound"]
         elif self.algorithm == "textblob_base":
-            blob = TextBlob(text)
+            blob = TextBlob(doc)
             return blob.sentiment.polarity
 
-    def analyze_text(self, text):
+    def analyze_doc(self, doc):
         '''
-        Analyzes a given text and returns a tuple
-        (text, predicted sentiment, polarity score)
-        where text is the original text;
+        Analyzes a given doc and returns a tuple
+        (doc, predicted sentiment, polarity score)
+        where doc is the original doc;
         predicted sentiment can be 'pos', 'neu' or 'neg'
         for positive, neutral and negative sentiment respectevely;
         and polarity score is a float that ranges from -1 to 1.
         '''
 
-        score = self.get_polarity_score(text)
+        score = self.get_polarity_score(doc)
         if score >= -1 and score < self.neu_inf_lim:
             predicted_sentiment = "neg"
         elif score >= self.neu_inf_lim and score < self.neu_sup_lim:
             predicted_sentiment = "neu"
         else:
             predicted_sentiment = "pos"
-        return (text, predicted_sentiment, score)
+        return (doc, predicted_sentiment, score)
 
-    def analyze_docs(self, text_list):
+    def analyze_docs(self, docs):
         '''
-        Analyzes a document collection by applying the analyze_text()
+        Analyzes a document collection by applying the analyze_doc()
         method for each document.
         All the results are stored in the _tagged_docs attribute.
         '''
 
         results = []
-        for text in text_list:
-            results.append(self.analyze_text(text))
+        for doc in docs:
+            results.append(self.analyze_doc(doc))
         self._tagged_docs = results
 
     @property
-    def tagged_words(self):
+    def tagged_docs(self):
         return self._tagged_docs
