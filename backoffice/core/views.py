854d06e5686f5fb7985257ad21a2340118522c5b
Jammily Ortigoza <jortigozaf@gmail.com>
2017-10-30 16:26:46 -0300

    Add JSON Web Tokens and Django REST Swagger * Add JWT for authentication * Add Swagger Api Documentation * Enable Django Admin Site * Create custom permissions and groups for users * Create a custom schema for Swagger

--- backoffice/core/views.py
+++ backoffice/core/views.py
@@ -1,17 +1,22 @@
 from rest_framework import viewsets
 from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.decorators import permission_classes
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import Group, Permission
 from core.models import (
     User, Project, Dataset, Visibility, Analysis, Algorithm, Visualization,
-    VisualizationType
+    VisualizationType, Ownership
 )
 from core.serializers import (
     UserSerializer, ProjectSerializer, DatasetSerializer,
     VisibilitySerializer, AnalysisSerializer, AlgorithmSerializer,
-    VisualizationSerializer, VisualizationTypeSerializer
+    VisualizationSerializer, VisualizationTypeSerializer, OwnershipSerializer,
+    GroupSerializer, PermissionSerializer
 )
+from core.permissions import CorePermissions, CorePermissionsOrAnonReadOnly
 
 
+@permission_classes((CorePermissions, ))
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -23,40 +28,68 @@ class UserViewSet(viewsets.ModelViewSet):
 
     # hash passwords after updating an user
     def perform_update(self, serializer):
-        password = make_password(self.request.data['password'])
-        serializer.save(password=password)
+        if 'password' in self.request.data:
+            password = make_password(self.request.data['password'])
+            serializer.save(password=password)
+        else:
+            serializer.save()
 
 
+@permission_classes((CorePermissions, ))
 class VisibilityViewSet(viewsets.ModelViewSet):
     queryset = Visibility.objects.all()
     serializer_class = VisibilitySerializer
 
 
+@permission_classes((CorePermissions, ))
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all().prefetch_related('dataset')
     serializer_class = ProjectSerializer
 
 
+@permission_classes((CorePermissions, ))
 class DatasetViewSet(viewsets.ModelViewSet):
     queryset = Dataset.objects.all()
     serializer_class = DatasetSerializer
 
 
+@permission_classes((CorePermissionsOrAnonReadOnly, ))
 class AnalysisViewSet(viewsets.ModelViewSet):
     queryset = Analysis.objects.all()
     serializer_class = AnalysisSerializer
 
 
+@permission_classes((CorePermissionsOrAnonReadOnly, ))
 class AlgorithmViewSet(viewsets.ModelViewSet):
     queryset = Algorithm.objects.all()
     serializer_class = AlgorithmSerializer
 
 
+@permission_classes((CorePermissions, ))
 class VisualizationViewSet(viewsets.ModelViewSet):
     queryset = Visualization.objects.all()
     serializer_class = VisualizationSerializer
 
 
+@permission_classes((CorePermissions, ))
 class VisualizationTypeViewSet(viewsets.ModelViewSet):
     queryset = VisualizationType.objects.all()
     serializer_class = VisualizationTypeSerializer
+
+
+@permission_classes((CorePermissions, ))
+class OwnershipViewSet(viewsets.ModelViewSet):
+    queryset = Ownership.objects.all()
+    serializer_class = OwnershipSerializer
+
+
+@permission_classes((CorePermissions, ))
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+@permission_classes((CorePermissions, ))
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
