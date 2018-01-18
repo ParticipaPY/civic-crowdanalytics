6e04f3602bf9951f04da498a91c37d343c6e0849
Marcelo Alcaraz <marce.mmad@gmail.com>
2018-01-18 17:32:02 -0300

    Add lexicon-based Sentiment Analysis for spanish text. Add lexicons for polarity and lemmatizer

--- backoffice/analytics/sentiment_analysis.py
+++ backoffice/analytics/sentiment_analysis.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import nltk
+import os
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
 from analytics.utils import tokenize_and_remove_stop_words, tokenize_and_stem, \
-                  clean_emojis, translate_docs
+                  clean_emojis, translate_doc
+# from lib.SentiStrength import SentiStrength
 
 
 class SentimentAnalyzer():
@@ -28,11 +30,12 @@ class SentimentAnalyzer():
 
     algorithm : string, 'ntlk_vader' by default
         The algorithm used to calculate the polarity score of a doc.
-        Immplemented algorithms: 'ntlk_vader', 'textblob_base'
+        Immplemented algorithms: 'ntlk_vader', 'textblob_base', 
+        'ML-Senticon' (only for spanish language)
 
     language: string, 'english'; by default
         Language on which documents are written
-        Implemented languages: 'spanish'
+        Implemented languages: 'spanish', 'english'
     '''
 
     _sia = SentimentIntensityAnalyzer()
@@ -46,7 +49,127 @@ class SentimentAnalyzer():
         self.neu_sup_lim = neu_sup_lim
         self.algorithm = algorithm
         self.language=language
+        self.translate = False
+        self.need_normalization = False
+        if language == "spanish":
+            self.load_spa_resources()
 
+
+    def load_spa_resources(self):
+        '''
+        Load lexicons required when analyzing text in spanish.
+        - Michal Boleslav Měchura's Spanish lemmatization dictionary.
+        - ML SentiCon: Cruz, Fermín L., José A. Troyano, Beatriz Pontes, 
+          F. Javier Ortega. Building layered, multilingual sentiment 
+          lexicons at synset and lemma levels, 
+          Expert Systems with Applications, 2014.
+        
+        '''
+        dir = os.path.dirname(os.path.realpath(__file__)) + "/lexicon_lib"
+        fl = open(dir+"/lemmatization-es.txt")
+        lines = fl.readlines()
+        self.spa_lemmas = [(l.split()[0], l.split()[1]) for l in lines]
+        fl.close()
+        # Positive and negative word list not used for now
+        # Kept for possible future improvements
+        # fp = open(dir+"/lib/isol_positivas_mejorada.csv")
+        # self.isol_pos = [w.replace("\n", "") for w in fp.readlines()]
+        # fp.close()
+        # fn = open(dir+"/lib/isol_negativas_mejorada.csv")
+        # self.isol_neg = [w.replace("\n", "") for w in fn.readlines()]
+        # fn.close()
+        fmd = open(dir+"/MLsenticon.es.xml",  encoding='utf-8')
+        self.mlsent = {}
+        for l in fmd.readlines():
+            sl = l.split()
+            if len(sl) == 6:
+                word = sl[4].replace("_", " ")
+                pol = float(sl[2].split('"')[1])
+                self.mlsent[word] = pol
+        fmd.close()
+        self.min_score = 100
+        self.max_score = -100
+        if self.algorithm == "ML-Senticon":
+            self.need_normalization = True
+
+
+    def lemmatize_spa(self, spa_word):
+        '''
+        Return spanish lemma for a given word
+
+        Parameters
+        ----------
+        spa_word : string
+            Spanish word to lemmatize
+        '''
+        # spa_word is a word form
+        res1 = [i[0] for i in self.spa_lemmas if i[1]==spa_word]
+        if (len(res1)==1):
+            return res1[0]
+        # spa_word is already a lemma
+        res2 = [i[0] for i in self.spa_lemmas if i[0]==spa_word]
+        if (len(res2)>0):
+            return res2[0]
+        return ""
+
+
+    def spa_polarity_score(self, doc):
+        '''
+        Calculate a polarity score for a given doc usin ML-Senticon
+
+        Parameters
+        ----------
+        doc : string
+            Text to score
+
+        Returns
+        -------
+        mlsscore : float
+            Polarity score for the input doc (not normalized)
+        '''
+        n_pos = 0
+        n_neg = 0
+        mlsscore = 0
+        for word in doc.split():
+            lem_word = self.lemmatize_spa(word)
+            # iSol lists not used for now
+            # Kept for possible future improvements
+            # if word in self.isol_pos or lem_word in self.isol_pos:
+            #     n_pos = n_pos + 1
+            # if word in self.isol_neg or lem_word in self.isol_neg:
+            #     n_neg = n_neg + 1
+            if word in self.mlsent.keys():
+                mlsscore = mlsscore + self.mlsent[word]
+            elif lem_word in self.mlsent.keys():
+                mlsscore = mlsscore + self.mlsent[lem_word]
+        if mlsscore > self.max_score:
+            self.max_score = mlsscore
+        if mlsscore < self.min_score:
+            self.min_score = mlsscore
+        return mlsscore
+
+
+    def normalize_scores(self, results):
+        '''
+        Normalice polarity scores into the range [-1,1] and 
+        recalculates predicted sentiment label according to
+        the normalized score.
+        '''
+        normalized = []
+        max = self.max_score
+        min = self.min_score
+        for (doc, sentiment, score) in results:
+            n_score = -1 + ((score-min)*2)/(max-min)
+            if n_score < self.neu_inf_lim:
+                n_sentiment = "neg"
+            elif n_score >= self.neu_inf_lim and n_score < self.neu_sup_lim:
+                n_sentiment = "neu"
+            else:
+                n_sentiment = "pos"
+            normalized.append((doc, n_score, n_sentiment))
+        return normalized
+
+        
     def get_polarity_score(self, doc):
         '''
         Returns the polarity score for a given doc.
@@ -60,6 +183,18 @@ class SentimentAnalyzer():
         elif self.algorithm == "textblob_base":
             blob = TextBlob(doc)
             return blob.sentiment.polarity
+        # SentiStrength not supported anymore because is not open source
+        # elif self.algorithm == "SentiStrength":
+        #     if self.language == "english":
+        #         lang = "en"
+        #     elif self.language == "spanish":
+        #         lang = "es"
+        #     elif self.language == "french":
+        #         lang = "fr"
+        #     ss_score = SentiStrength.getSentimentScore(doc, lang)
+        #     return ss_score/4
+        elif self.algorithm == "ML-Senticon":
+            return self.spa_polarity_score(doc)
 
     def analyze_doc(self, doc):
         '''
@@ -70,10 +205,16 @@ class SentimentAnalyzer():
         for positive, neutral and negative sentiment respectevely;
         and polarity score is a float that ranges from -1 to 1.
         '''
-        
-        pp_doc = tokenize_and_remove_stop_words(text=doc, join_words=True)
+        # pre processing stage
+        pp_doc = clean_emojis(doc)
+        if self.translate:
+            pp_doc = translate_doc(doc, src=self.src_lang, dest="en")
+        pp_doc = tokenize_and_remove_stop_words(text=pp_doc, join_words=True,
+                                                language=self.language)
+        # get polarity score from pre processed doc
         score = self.get_polarity_score(pp_doc)
-        if score >= -1 and score < self.neu_inf_lim:
+        # determine polarity from score and thresholds
+        if score < self.neu_inf_lim:
             predicted_sentiment = "neg"
         elif score >= self.neu_inf_lim and score < self.neu_sup_lim:
             predicted_sentiment = "neu"
@@ -87,29 +228,27 @@ class SentimentAnalyzer():
         method for each document.
         All the results are stored in the _tagged_docs attribute.
         Emojis are removed from docs.
-        If the docs' language is not english, they are translated to
-        english to use the english sentiment lexicon of the analyzer.
-        If the docs' language is not english, after analyze sentiment
+        If the docs' language is not english and the algorithm is only for
+        english docs, they are translated to english to use the english 
+        sentiment lexicon of the analyzer.
         '''
 
         results = []
-        docs = clean_emojis(docs)
-        if (self.language != "english"):
+        if (self.algorithm == "nltk_vader" or self.algorithm == "textblob_base")\
+            and self.language != "english":
+            self.translate = True
             if (self.language == "spanish"):
-                src = "es"
-            translations = translate_docs(docs, src=src, dest="en")
-            originals = [o for (o,t) in translations]
-            translated = [t for (o,t) in translations]
-        else:
-            translated = docs
-        for doc in translated:
-            results.append(self.analyze_doc(doc))
+                self.src_lang = "es"
+            elif (self.language == "french"):
+                self.src_lang = "fr"
         
-        if (self.language != "english"):
-        # translation might change docs order
-        # original non translated text must be re inserted in the results
-            results = [(originals[i], results[i][1], results[i][2]) for i in range(len(results))]
+        for doc in docs:
+            results.append(self.analyze_doc(doc))
+
+        if self.need_normalization:
+            results = self.normalize_scores(results)
         self._tagged_docs = results
+
 
     @property
     def tagged_docs(self):
